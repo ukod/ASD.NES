1ed32a781250a1cb72c977c05d997047e345dca9
Stanislav Kuzmich [Art.Stea1th] <art.stea1th@gmail.com>
2018-02-25 05:02:53 +0300

    initial

---
+++ Application/ASD.NES.Infrastructure/Interfaces.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// propotyping
+
+
+namespace ASD.NES.Infrastructure {
+
+    // logic
+
+    public interface IProcessingUnit {
+        void Step();
+        void Step(int times);
+    }
+
+    // memory management
+
+    public abstract class Memory<T> : IMemory<T> where T : struct {
+
+        private IList<T> memory;
+        private IReadOnlyCollection<RedirectRule> rules;
+
+        public T this[int address] {
+            get => memory[Redirect(address)];
+            set => memory[Redirect(address)] = value;
+        }
+
+        private int Redirect(int address)
+            => rules.First(r => r.SourceAddress == address).TargetAddress;
+
+        // configure
+        // add offset
+        // remap single address
+        // remap range
+        // ... move this to separate mapper? and after configure mem.mapper = mapper?
+
+
+        private void Test() {
+
+        }
+
+    }
+
+    public interface IMemory<T> where T : struct {
+        T this[int address] { get; set; }
+    }
+
+    public struct RedirectRule {
+        public int SourceAddress { get; }
+        public int TargetAddress { get; }
+        public RedirectRule(int sourceAddress, int targetAddress) {
+            SourceAddress = sourceAddress; TargetAddress = targetAddress;
+        }
+    }
+}
